5d390a732719730fbe95c1fa275430b88451aac1
ben smith <benjaminsmith1981@gmail.com>
2017-10-21 14:24:07 +0200

    added port info

--- Sources/myFirstProject/main.swift
+++ Sources/myFirstProject/main.swift
@@ -1,12 +1,28 @@
 import Kitura
 import MongoKitten
 import HeliumLogger
+import Foundation
+
 
 // Create a new router
 let router = Router()
 
 // Initialize HeliumLogger
 HeliumLogger.use()
+
+// Resolve the port that we want the server to listen on.
+let port: Int
+let defaultPort = 8080
+if let requestedPort = ProcessInfo.processInfo.environment["PORT"] {
+    port = Int(requestedPort) ?? defaultPort
+} else {
+    port = defaultPort
+}
+
+let mongoDB: Database?
+mongoDB = try Database("mongodb://benterrysmith:benterrysmith@ds227035.mlab.com:27035/heroku_cfng8982")
+// Add your application here
+print("Connected to MongoDB")
 
 // Handle HTTP GET requests to /
 router.get("/hello/:name") {
